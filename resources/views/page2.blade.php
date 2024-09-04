2cabd1b37525f62e8136c166e108c549f4170391
sahid10 <aysahid@gmail.com>
2024-09-04 09:27:06 +0700

    add template

---
+++ resources/views/page2.blade.php
@@ -0,0 +1,11 @@
+@extends('template')
+@section('content')
+    
+
+<!-- content -->
+<div class="container-fluid py-4">
+<h1>page2</h1>
+
+  </div>
+  <!-- end content -->
+  @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+    // return view('material-dashboard-master.pages/sign-in');
+});
+
+Route::get('page2', function () {
+    return view('page2');
 });
 
 Route::get('/counter', Counter::class);
